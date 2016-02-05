e9d364198f857e69d8672e5b34652a34cecac249
timburbank <tim@timburbank.com>
2016-02-05 03:58:51 -0500

    initial prototyping of main functionality

---
+++ lib/db.py
@@ -0,0 +1,16 @@
+# Functions for interacting with the database
+
+
+
+def database():
+	print('Date a Bass?')
+
+	
+	
+	
+
+# returns list of tickets as list of dictionaries 
+def get_list()
+	list = []
+	
+	
