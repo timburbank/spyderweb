9f0500f4a5a50fe877a378e5abc5b33cef9d939f
timburbank <tim@timburbank.com>
2016-02-06 04:52:48 -0500

    Renamed db.py to data.py, and get_ticket_data() now returns list of dictionaries. list() kinda works

---
+++ lib/data.py
@@ -0,0 +1,33 @@
+# Functions for interacting with the database
+
+# Confirm we're in the right file
+def database():
+	print('Date a Bass?')
+
+# params
+# fields: list of ticket fields to retriev
+# filters: dictionary of field:value:comparison
+# order: string, field to order by
+# limit: int, how many items to retrieve
+# ascending: bool, which way to order
+def get_ticket_data(fields, filters = 0, order='id', ascending = 1, limit = 0):
+
+	temp_data_set = [ \
+		{'id':1, 'name':'Garnet', 	'description':'We are the crystal gems',	'status':'todo'}, \
+		{'id':2, 'name':'Amethyst', 'description':"We'll always save the day",	'status':'complete'}, \
+		{'id':3, 'name':'Pearl', 	'description':"And if you think we can't",	'status':'workable'}, \
+		{'id':4, 'name':'Stephen', 	'description':"We'll always find a way",	'status':'todo'} \
+	]
+	
+	data = []
+	
+	for item in temp_data_set:
+		datum = {}
+		for field in fields:
+			datum[field] = item[field]
+		data.append(datum)
+		
+		# need to implement filters
+		
+	return(data)
+	
--- spyderweb.py
+++ spyderweb.py
@@ -1,26 +1,26 @@
-from lib import db
+from lib import data
 
 
 def list():
-	data = db.get('ticket')
-	for item in data:
-		print(item)
-	
-	list =  \
-		'number  name                status \n' \
-    		'1       first ticket        new \n' \
-		'2       second ticket       closed \n' \
-		'3       third ticket        kinda workable \n' \
+	fields = ['id', 'name', 'status']
+	ticket_data = data.get_ticket_data(fields)
+	list = ''
+	for ticket in ticket_data:
+		list = '{}{:<3}{:24}{}\n'.format(list, ticket['id'], ticket['name'], ticket['status'])
+	print(list)
     
     
-def view():
+def view(id):
+	fields = ['id', 'name', 'status', 'description']
+	ticket_data = data.get_ticket_data(fields)
 	
 	view = \
-		'#1 Ticket Name \n' \
+		'{} {} \n' \
 		'============== \n' \
 		'\n' \
 		'We are the crystal gems / We\'ll always save the day / ' \
-		'And if you think we can\'t / We\'ll always find a way' 
+		'And if you think we can\'t / We\'ll always find a way' \
+		.format(ticket_data['id'], ticket_data['name'])
 		
 	print(view)
     
@@ -41,17 +41,19 @@ def delete():
 # handle command line inputs
 if __name__ == "__main__": # doesn't run if file is imported somewhere
 	import sys
-	arg = sys.argv[1] # get command line argument
+	
+	if(len(sys.argv) > 1):
+		arg = sys.argv[1] # get command line argument
 
-	if arg == 'list':
-		list()
-	elif arg == 'view':
-		view()
-	elif arg == 'create':
-		create()
-	elif arg == 'update':
-		update()
-	elif arg == 'hide':
-		hide()
-	elif arg == 'delete':
-		delete()
+		if arg == 'list':
+			list()
+		elif arg == 'view':
+			view(sys.argv[2])
+		elif arg == 'create':
+			create()
+		elif arg == 'update':
+			update()
+		elif arg == 'hide':
+			hide()
+		elif arg == 'delete':
+			delete()
