362941abed78b586c4d65096dcec4b9306970e63
Tim Burbank <tim@timburbank.com>
2016-02-22 16:15:26 -0500

    Use config file to set ticket fields and list fields

--- lib/config.py
+++ lib/config.py
@@ -1,21 +1,37 @@
 # Functions for reading config files, wherever those
 # config files happen to be
+import os
+import ConfigParser
 
 env = ""
 
 # This might need some kind of reference to the evironment
 
-def fields():
-	
+def get_value(section, option):
 	# note: id will always be a field
-	fields = ['name', 'description', 'status']
+	
+	# based on https://pymotw.com/2/ConfigParser/
+	config_file = os.path.join(env, 'spyderweb.conf')	
+	try:
+		parser = ConfigParser.SafeConfigParser()
+		parser.read(config_file)
+	except ConfigParser.ParsingError, err:
+		print 'Could not parse:', err
 
+	fields_csv = parser.get(section, option)
+	
+	fields = fields_csv.split(',')
+	clean_fields = map(str.strip, fields)
+	
+	return(clean_fields)
+
+def fields():
+	fields = get_value('tickets', 'fields')
 	return(fields)
-
 
 # returns fields shown when listing tickets (can maybe take
 # optional param for different views?)
 def list_fields():
-	list_fields = ['id', 'name', 'status']
+	list_fields = get_value('list', 'list_fields')
 	return(list_fields)
 
